bba8b6db673cbd45363115c7ddf0ccf045720a07
tmdxo1274 <aahnn1223@gmail.com>
2022-04-25 14:36:25 +0900

    Car Class init

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,14 @@
+package racingcar;
+
+public class Car {
+    private String name;
+    private int position;
+
+    Car(){}
+    Car(String name, int position){
+        this.name = name;
+        this.position = position;
+    }
+
+
+}
