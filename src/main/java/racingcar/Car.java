b8ceb924e68cd130f5c039e96d8afaab3e9027a9
tmdxo1274 <aahnn1223@gmail.com>
2022-04-25 14:52:29 +0900

    Car name 원시값 포장

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,12 +1,12 @@
 package racingcar;
 
 public class Car {
-    private String name;
+    private CarName name;
     private int position;
 
     Car(){}
     Car(String name, int position){
-        this.name = name;
+        this.name = new CarName(name);
         this.position = position;
     }
 
